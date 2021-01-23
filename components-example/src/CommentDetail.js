2fe48203d034c7ec928e346e7575753bf7a9cbe1
Elvan H <elvanhd@gmail.com>
2021-01-23 14:53:56 +0700

    passing and receiving props

--- components-example/src/CommentDetail.js
+++ components-example/src/CommentDetail.js
@@ -1,17 +1,14 @@
 import React from 'react';
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
   return (
     <div className='comment'>
       <a href='/' className='avatar'>
-        <img
-          alt='avatar'
-          src='https://images.unsplash.com/photo-1611119999058-882233e88d35?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=100&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=100'
-        />
+        <img alt='avatar' src={props.imageUrl} />
       </a>
       <div className='content'>
         <a href='/' className='author'>
-          Sam
+          {props.author}
         </a>
         <div className='metadata'>
           <span className='date'>Today at 6:00PM</span>
--- components-example/src/index.js
+++ components-example/src/index.js
@@ -5,11 +5,18 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className='ui container comments'>
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail
+        author='Sam'
+        imageUrl='https://images.unsplash.com/photo-1611119999058-882233e88d35?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=100&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=100'
+      />
+      <CommentDetail
+        author='Alexa'
+        imageUrl='https://images.unsplash.com/photo-1611080466336-664df15e5d79?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=100&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=100'
+      />
+      <CommentDetail
+        author='Jane'
+        imageUrl='https://images.unsplash.com/photo-1611272785707-d9520d586f89?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=100&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=100'
+      />
     </div>
   );
 };
