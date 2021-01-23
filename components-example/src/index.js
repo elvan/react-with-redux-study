e84b88e6c7e15f113391ffd457ae8e334aff712f
Elvan H <elvanhd@gmail.com>
2021-01-23 15:18:38 +0700

    passing props

--- components-example/src/index.js
+++ components-example/src/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className='ui container comments'>
       <CommentDetail
-        author='Sam'
-        imageUrl='https://images.unsplash.com/photo-1611119999058-882233e88d35?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=100&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=100'
+        author={faker.name.findName()}
+        timeAgo='Today at 4:45PM'
+        content={faker.lorem.sentence()}
+        avatar='http://placeimg.com/64/64/people'
       />
       <CommentDetail
-        author='Alexa'
-        imageUrl='https://images.unsplash.com/photo-1611080466336-664df15e5d79?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=100&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=100'
+        author={faker.name.findName()}
+        timeAgo='Today at 2:00AM'
+        content={faker.lorem.sentence()}
+        avatar='http://placeimg.com/64/64/fashion'
       />
       <CommentDetail
-        author='Jane'
-        imageUrl='https://images.unsplash.com/photo-1611272785707-d9520d586f89?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=100&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=100'
+        author={faker.name.findName()}
+        timeAgo='Yesterday at 5:00PM'
+        content={faker.lorem.sentence()}
+        avatar='http://placeimg.com/64/64/city'
       />
     </div>
   );
