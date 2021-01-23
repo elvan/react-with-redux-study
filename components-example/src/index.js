24de12d7b5cf35a32d911fde66e0833a84a8dd09
Elvan H <elvanhd@gmail.com>
2021-01-23 18:04:32 +0700

    implementing an approval card

--- components-example/src/index.js
+++ components-example/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className='ui container comments'>
+      <ApprovalCard />
       <CommentDetail
         author={faker.name.findName()}
         timeAgo='Today at 4:45PM'
