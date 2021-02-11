b195ea5daffc36e9fbdc6bd8d7cc3ea28450a849
Elvan H <elvanhd@gmail.com>
2021-02-12 01:21:43 +0700

    refactoring from functional to class components

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (error) => console.error(error),
-  );
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (error) => console.error(error),
+    );
 
-  return <div>Hi there!</div>;
-};
+    return <div>Hi there!</div>;
+  }
+}
 
 ReactDOM.render(
   <App />,
