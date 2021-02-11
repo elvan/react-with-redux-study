8b73ea97b587ea3ec6e5d48edcf439aabcf8aecc
Elvan H <elvanhd@gmail.com>
2021-02-12 00:10:32 +0700

    getting a user's physical location

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (error) => console.error(error),
+  );
+
   return <div>Hi there!</div>;
 };
 
