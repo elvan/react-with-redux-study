3c0931cf92fad67b8e8335eeb5444b32b215c599
Elvan H <elvanhd@gmail.com>
2021-02-15 19:04:16 +0700

    styling a list

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -5,7 +5,8 @@ const VideoList = ({ videos }) => {
   const renderedVideos = videos.map((video) => {
     return <VideoItem video={video} />;
   });
-  return <div>{renderedVideos}</div>;
+
+  return <div className="ui relaxed divided list">{renderedVideos}</div>;
 };
 
 export default VideoList;
