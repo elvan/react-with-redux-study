1e16e1e04e0f94c3a757e9d3c8138e63b5adb44f
Elvan H <elvanhd@gmail.com>
2021-02-19 23:17:22 +0700

    videos-hooks project setup

---
+++ videos-hooks/src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+import youtubeConfig from '../config/youtube-config';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResult: 5,
+    type: 'video',
+    key: youtubeConfig.key,
+  },
+});
