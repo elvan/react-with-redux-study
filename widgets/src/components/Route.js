0217ba8a6de122f3babf6180a55b213ba3fe5f8a
Elvan H <elvanhd@gmail.com>
2021-02-19 20:06:22 +0700

    updating the route

--- widgets/src/components/Route.js
+++ widgets/src/components/Route.js
@@ -1,18 +1,21 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const Route = ({ path, children }) => {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
   useEffect(() => {
     const onLocationChange = () => {
-      console.log('Location change');
+      setCurrentPath(window.location.pathname);
     };
+
     window.addEventListener('popstate', onLocationChange);
 
     return () => {
       window.removeEventListener('popstate', onLocationChange);
     };
-  });
+  }, []);
 
-  return window.location.pathname === path ? children : null;
+  return currentPath === path ? children : null;
 };
 
 export default Route;
