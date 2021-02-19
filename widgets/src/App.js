d4448c691d7510ddbf754337cdebb0ce4ecae539
Elvan H <elvanhd@gmail.com>
2021-02-19 11:44:14 +0700

    building a reusable route component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import Translate from './components/Translate';
 import Accordion from './components/Accordion';
+import Dropdown from './components/Dropdown';
+import Route from './components/Route';
 import Search from './components/Search';
+import Translate from './components/Translate';
 
 // Accordion items
 const items = [
@@ -41,30 +43,28 @@ const options = [
   },
 ];
 
-const showAccordion = () => {
-  if (window.location.pathname === '/') {
-    return <Accordion items={items} />;
-  }
-};
-
-const showList = () => {
-  if (window.location.pathname === '/list') {
-    return <Search />;
-  }
-};
-
-const showTranslate = () => {
-  if (window.location.pathname === '/translate') {
-    return <Translate />;
-  }
-};
-
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      {showAccordion()}
-      {showList()}
-      {showTranslate()}
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
