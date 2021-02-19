e6e4dbb5837198fcdaee433ef61b2cdb3044b444
Elvan H <elvanhd@gmail.com>
2021-02-19 20:01:37 +0700

    detecting navigation

--- widgets/src/components/Link.js
+++ widgets/src/components/Link.js
@@ -4,6 +4,9 @@ const Link = ({ href, className, children }) => {
   const onClick = (event) => {
     event.preventDefault();
     window.history.pushState({}, ',', href);
+
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
   };
 
   return (
