f2ad2dccf97d102882a317dcd716438d0b779b3a
Elvan H <elvanhd@gmail.com>
2021-02-23 13:55:58 +0700

    creating postsReducers

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import postsReducers from './postsReducers';
 
 export default combineReducers({
-  dummy: () => 'Hi there',
+  posts: postsReducers,
 });
---
+++ blog/src/reducers/postsReducers.js
@@ -0,0 +1,5 @@
+const postsReducers = () => {
+  return 123;
+};
+
+export default postsReducers;
