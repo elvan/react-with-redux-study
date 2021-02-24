5bde8dd540b1049761fc27ef0cc6cbbeb6d54764
Elvan H <elvanhd@gmail.com>
2021-02-24 11:36:42 +0700

    finding unique user ids

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,9 +1,11 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
 
-export const fetchPostsAndUsers = () => async (dispatch) => {
-  console.log('About to fetch posts');
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
-  console.log('Fetched posts');
+
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
 };
 
 export const fetchPosts = () => async (dispatch) => {
