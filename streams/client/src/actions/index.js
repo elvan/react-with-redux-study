f20381e0ba538f4c6f92eb76a9140d61e399f11c
Elvan H <elvanhd@gmail.com>
2021-02-28 22:32:47 +0700

    bulk action creators

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,5 +1,13 @@
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
 import streams from '../apis/streams';
+import {
+  CREATE_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  SIGN_IN,
+  SIGN_OUT,
+} from './types';
 
 export const signIn = (userId) => {
   return {
@@ -20,5 +28,43 @@ export const createStream = (formValues) => async (dispatch) => {
   dispatch({
     type: CREATE_STREAM,
     payload: response.data,
+  });
+};
+
+export const fetchStreams = () => async (dispatch) => {
+  const response = await streams.get('/streams');
+
+  dispatch({
+    type: FETCH_STREAMS,
+    payload: response.data,
+  });
+};
+
+export const fetchStream = (id) => async (dispatch) => {
+  const response = await streams.post(`/streams/${id}`);
+
+  dispatch({
+    type: FETCH_STREAM,
+    payload: response.data,
+  });
+};
+
+export const editStream = (id, formValues) => async (dispatch) => {
+  const response = await streams.put(`/streams/${id}`, {
+    params: formValues,
+  });
+
+  dispatch({
+    type: EDIT_STREAM,
+    payload: response.data,
+  });
+};
+
+export const deleteStream = (id) => async (dispatch) => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({
+    type: DELETE_STREAM,
+    payload: id,
   });
 };
--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -3,10 +3,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
-import reduxThunk from 'redux-thunk';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
