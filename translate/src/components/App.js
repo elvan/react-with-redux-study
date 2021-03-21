14625c149bd8ae1de2de3f9e73cf337bfb496636
Elvan H <elvanhd@gmail.com>
2021-03-21 22:43:51 +0700

    connecting the selector, the field and button to the store

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import UserCreate from './UserCreate';
-import LanguageSelector from './LanguageSelector';
-import LanguageContext from '../context/LanguageContext';
 import ColorContext from '../context/ColorContext';
+import { LanguageStore } from '../context/LanguageContext';
+import LanguageSelector from './LanguageSelector';
+import UserCreate from './UserCreate';
 
 class App extends React.Component {
-  state = { language: 'english' };
-
-  onLanguageChange = (language) => {
-    this.setState({ language });
-  };
-
   render() {
     return (
       <div className="ui container">
-        <LanguageSelector onLanguageChange={this.onLanguageChange} />
-        <ColorContext.Provider value="red">
-          <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContext.Provider value="red">
             <UserCreate />
-          </LanguageContext.Provider>
-        </ColorContext.Provider>
+          </ColorContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
